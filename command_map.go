248c4116009f4af966ee42050173475692f48fca
Thanakon <kimsanthanakon@gmail.com>
2024-08-18 18:05:17 +1000

    feat: add mapb command

--- command_map.go
+++ command_map.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func commandMap(cfg *config) error {
+func commandMapf(cfg *config) error {
 	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsUrl)
+	if err != nil {
+		return err
+	}
+
+	cfg.nextLocationsUrl = locationsResp.Next
+	cfg.previousLocationsUrl = locationsResp.Previous
+
+	for _, loc := range locationsResp.Results {
+		fmt.Println(loc.Name)
+	}
+
+	return nil
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.previousLocationsUrl == nil {
+		return errors.New("no previous locations found")
+	}
+
+	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.previousLocationsUrl)
 	if err != nil {
 		return err
 	}

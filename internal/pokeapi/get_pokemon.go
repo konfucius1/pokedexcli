21f6e48bf4511ae9b13e8ef5d2a7dfe586dd1db1
Thanakon <kimsanthanakon@gmail.com>
2024-08-18 23:15:48 +1000

    feat: add get pokemon endpoint

---
+++ internal/pokeapi/get_pokemon.go
@@ -0,0 +1,46 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func (c *Client) GetPokemon(name string) (Pokemon, error) {
+	url := baseURL + "/pokemon/" + name
+
+	// check cache
+	if val, ok := c.cache.Get(url); ok {
+		pokemonResp := Pokemon{}
+		err := json.Unmarshal(val, &pokemonResp)
+		if err != nil {
+			return Pokemon{}, err
+		}
+		return pokemonResp, nil
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	pokemon := Pokemon{}
+	err = json.Unmarshal(data, &pokemon)
+	if err != nil {
+		return Pokemon{}, err
+	}
+
+	return pokemon, nil
+}

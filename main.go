5d66fbce0aa104a26e9876da2358cee8a20acc8a
Thanakon <kimsanthanakon@gmail.com>
2024-08-18 16:50:51 +1000

    feat: add pokeclient and config

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"github.com/konfucius1/pokedexcli/internal/pokeapi"
+	"time"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+	startRepl(cfg)
 }

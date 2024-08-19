619e855e4d35004125bdf310b3cc75b9185432ba
Thanakon <kimsanthanakon@gmail.com>
2024-08-19 15:04:53 +1000

    feat: add pokedex command

--- command_catch.go
+++ command_catch.go
@@ -36,5 +36,7 @@ func commandCatch(cfg *config, args ...string) error {
 
 	cfg.caughtPokemon[pokemon.Name] = pokemon
 
+	fmt.Println("You may now inspect it with the inspect command.")
+
 	return nil
 }
